97eebab60df4d1f88a47e285a180b4d4ce521b4a
Pienaar, Adriaan H <adriaan.h.pienaar@accenture.com>
2019-04-29 15:09:14 +0200

    Updates

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -17,42 +17,42 @@ import { UserService } from '../user.service';
 export class EditProfilePage implements OnInit {
 
 
-mainuser: AngularFirestoreDocument;
-sub;
-username: string;
-profilePic: string;
-cellNumber: string;
-name: string;
-surname: string;
-email: string;
-phone: string;
-occupation: string;
-location: string;
-level: string;
-data;
+  mainuser: AngularFirestoreDocument;
+  sub;
+  username: string;
+  profilePic: string;
+  cellNumber: string;
+  name: string;
+  surname: string;
+  email: string;
+  phone: string;
+  occupation: string;
+  location: string;
+  level: string;
+  data;
 
-// tslint:disable-next-line: no-inferrable-types
-busy: boolean = false;
+  // tslint:disable-next-line: no-inferrable-types
+  busy: boolean = false;
 
   @ViewChild('fileBtn') fileBtn: {
-  nativeElement: HTMLInputElement
-};
+    nativeElement: HTMLInputElement
+  };
 
 
-constructor(
-private http: Http,
-private afs: AngularFirestore,
-private router: Router,
-private alertController: AlertController,
-private user: UserService,
+  constructor(
+    private http: Http,
+    private afs: AngularFirestore,
+    private router: Router,
+    private alertController: AlertController,
+    private user: UserService,
 
-public alertCtrl: AlertController) {
-this.mainuser = afs.doc(`members/${user.getUID()}`);
-this.sub = this.mainuser.valueChanges().subscribe(event => {
-this.username = event.username;
-this.profilePic = event.profilePic;
-this.data = event.data;
-});
+    public alertCtrl: AlertController) {
+    this.mainuser = afs.doc(`members/${user.getUID()}`);
+    this.sub = this.mainuser.valueChanges().subscribe(event => {
+      this.username = event.username;
+      this.profilePic = event.profilePic;
+      this.data = event.data;
+    });
 
   }
 
@@ -60,104 +60,107 @@ this.data = event.data;
   ngOnInit() {
   }
 
-// tslint:disable-next-line: use-life-cycle-interface
+  // tslint:disable-next-line: use-life-cycle-interface
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-updateProfilePic() {
-this.fileBtn.nativeElement.click();
-}
-
-uploadPic(event) {
-  const files = event.target.files;
-
-  const data = new FormData();
-  data.append('file', files[0]);
-  data.append('UPLOADCARE_STORE', '1');
-  data.append('UPLOADCARE_PUB_KEY', 'ba1eb415661523be4eba');
-  this.http.post('https://upload.uploadcare.com/base/', data)
-// tslint:disable-next-line: no-shadowed-variable
-  .subscribe(event => {
-    const uuid = event.json().file;
-    this.mainuser.update({
-      profilePic: uuid,
-    });
-  });
-}
-
-async createPost() {
-  this.busy = true;
-
-  const name = this.name;
-  const cellnumber = this.cellNumber;
-  const email = this.email;
-  const phone = this.phone;
-  const occupation = this.occupation;
-  const location = this.location;
-  const level = this.level;
-
-  this.afs.doc(`members/${this.user.getUID()}`).update({
-    data: { name,
-    cellnumber,
-    email,
-    phone,
-    occupation,
-    location,
-    level
-}
-  });
-
-  this.router.navigate(['/tabs/profile']);
+  updateProfilePic() {
+    this.fileBtn.nativeElement.click();
   }
 
-async presentAlert(title: string, content: string) {
-  const alert = await this.alertController.create({
-    header: title,
-    message: content,
-    buttons: ['OK']
-  });
+  uploadPic(event) {
+    const files = event.target.files;
 
-  await alert.present();
-}
-
-async updateDetails() {
-
-  if (this.username !== this.user.getUsername()) {
-    await this.user.updateEmail(this.username);
-    this.mainuser.update({
-      username: this.username
-    });
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('UPLOADCARE_STORE', '1');
+    data.append('UPLOADCARE_PUB_KEY', 'ba1eb415661523be4eba');
+    this.http.post('https://upload.uploadcare.com/base/', data)
+      // tslint:disable-next-line: no-shadowed-variable
+      .subscribe(event => {
+        const uuid = event.json().file;
+        this.mainuser.update({
+          profilePic: uuid,
+        });
+      });
   }
 
-  await this.presentAlert('Done!', 'Your profile was updated!');
+  async createPost() {
+    this.busy = true;
 
-  this.router.navigate(['/tabs/profile']);
-}
-async presentAlertConfirm() {
-  const alert = await this.alertCtrl.create({
-    header: 'Cancel?',
-    message: 'Would you like to cancel the changes that you have made?',
-    buttons: [
-      {
-        text: 'No',
-        role: 'cancel',
-        cssClass: 'secondary',
-        handler: (blah) => {
-          console.log('Confirm Cancel: blah');
-        }
-      }, {
-        text: 'Yes',
-        handler: () => {
-          this.router.navigate(['/tabs/profile']);
-        }
+    const name = this.name;
+    const cellnumber = this.cellNumber;
+    const email = this.email;
+    const phone = this.phone;
+    const occupation = this.occupation;
+    const location = this.location;
+    const level = this.level;
+
+    this.afs.doc(`members/${this.user.getUID()}`).update({
+      data: {
+        name,
+        cellnumber,
+        email,
+        phone,
+        occupation,
+        location,
+        level
       }
-    ]
-// tslint:disable-next-line: semicolon
-  });
-  await alert.present();
-}
+    });
+
+    this.router.navigate(['/tabs/profile']);
+  }
+
+  async presentAlert(title: string, content: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: content,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async updateDetails() {
+
+    if (this.username !== this.user.getUsername()) {
+      await this.user.updateEmail(this.username);
+      this.mainuser.update({
+        username: this.username
+      });
     }
+
+    await this.presentAlert('Done!', 'Your profile was updated!');
+
+    this.router.navigate(['/tabs/profile']);
+  }
+  async presentAlertConfirm() {
+    this.router.navigate(['/tabs/profile']);
+  }
+  //   const alert = await this.alertCtrl.create({
+  //     header: 'Cancel?',
+  //     message: 'Would you like to cancel the changes that you have made?',
+  //     buttons: [
+  //       {
+  //         text: 'No',
+  //         role: 'cancel',
+  //         cssClass: 'secondary',
+  //         handler: (blah) => {
+  //           console.log('Confirm Cancel: blah');
+  //         }
+  //       }, {
+  //         text: 'Yes',
+  //         handler: () => {
+  //           this.router.navigate(['/tabs/profile']);
+  //         }
+  //       }
+  //     ]
+  // // tslint:disable-next-line: semicolon
+  //   });
+  //   await alert.present();
+  // }
+}
 
 
 
